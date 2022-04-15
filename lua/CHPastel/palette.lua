e6eca2c3c549929d912cae33b4f86206541b28fe
Jacob <info@caasi.dev>
2022-04-15 22:44:11 +0100

    testing

--- lua/CHPastel/palette.lua
+++ lua/CHPastel/palette.lua
@@ -1,60 +1,52 @@
 local colors = {
     chpastel_bg = "#0f1c1f",
 
-    -- black, white and grey
     black = "#1C1C1C",
-    darkest_grey = "#3A3A3A", --
-    darker_grey = "#444444", --
-    dark_grey = "#626262", --
-    grey = "#808080", --
-    light_grey = "#9E9E9E" --,
-    lighter_grey = "#BCBCBC", --
-    lightest_grey = "#CCCCCC", --
+    darkest_grey = "#3A3A3A",
+    darker_grey = "#444444",
+    dark_grey = "#626262",
+    grey = "#808080",
+    light_grey = "#9E9E9E",
+    lighter_grey = "#BCBCBC",
+    lightest_grey = "#CCCCCC",
     white = "#898576",
 
-    -- yellow and orange
-    dark_orange = "#FF9A00", --
-    light_orange = "#FF9D00",--
+    dark_orange = "#FF9A00",
+    light_orange = "#FF9D00",
     yellow = "#806731",
-    light_yellow = "#F2ED7F",--
+    light_yellow = "#F2ED7F",
 
-    -- green
-    darkest_green = "#2D7067",--
-    dirty_green = "#70b950",--
+    darkest_green = "#2D7067",
+    dirty_green = "#70b950",
     green = "#416644",
-    light_green = "#88FF88",--
-    lighter_green = "#9EFF80",--
-    lightest_green = "#BBFFDD",--
+    light_green = "#88FF88",
+    lighter_green = "#9EFF80",
+    lightest_green = "#BBFFDD",
 
-    -- purple
-    dark_purple = "#345FA8",--
+    dark_purple = "#345FA8",
     purple = "#6a4771",
-    light_purple = "#DEEBFE",--
+    light_purple = "#DEEBFE",
 
-    -- blue
-    darkest_blue = "#0000df",--
-    darker_blue = "#0050A4",--
-    dark_blue = "#0088FF",--
+    darkest_blue = "#0000df",
+    darker_blue = "#0050A4",
+    dark_blue = "#0088FF",
     blue = "#326777",
     light_blue = "#317389",
-    greyish_blue = "#8fbfdc",--
-    dirty_blue = "#668799",--
+    greyish_blue = "#8fbfdc",
+    dirty_blue = "#668799",
 
-    -- red
-    aubergine = "#4F0037",--
-    darker_red = "#700009",--
-    dark_red = "#902020",--
+    aubergine = "#4F0037",
+    darker_red = "#700009",
+    dark_red = "#902020",
     red = "#814b49",
 
-    -- pink
-    dark_pink = "#FF628C",---
-    pink = "#FF00FF",--
-    light_pink = "#EE80E1",--
-    lightest_pink = "#FFA5F3",--
-    pale_pink = "#F0A0C0",--
-    dirty_pink = "#EB939A",--
+    dark_pink = "#FF628C",-
+    pink = "#FF00FF",
+    light_pink = "#EE80E1",
+    lightest_pink = "#FFA5F3",
+    pale_pink = "#F0A0C0",
+    dirty_pink = "#EB939A",
 
-    -- cursor line
     cursor_line = "#112940",
     cursor_hover = "#112940",
 }
